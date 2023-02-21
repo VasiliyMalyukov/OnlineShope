98368ae7b2e5c75517b9fbedebbfa775808f834b
Wovasya <v37444170895@gmail.com>
2023-02-21 21:00:03 +0400

    Finished project

--- src/main/java/ru/netology/javaqamvn/catalog/domain/Product.java
+++ src/main/java/ru/netology/javaqamvn/catalog/domain/Product.java
@@ -15,23 +15,8 @@ public class Product {
         return id;
     }
 
-    public void setID(int ID) {
-        this.id = ID;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
 }
